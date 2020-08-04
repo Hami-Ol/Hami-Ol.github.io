7c6955e6e488d315ccd377b0be77d3abc080357f
Hami <hami@Hamis-MacBook-Pro.local>
2020-08-04 07:07:22 -0500

    weekthree

---
+++ W3D1/testing.js
@@ -0,0 +1,100 @@
+describe("Account", function(){
+    let acc=new Account(1);
+    
+   
+    it("getNumber of Account", function(){
+        assert.equal(100, new Account(100).getNumber());
+    });
+
+    it("Deposit of Account ", function(){
+        acc.deposit(300);
+        assert.equal(300, acc.getBalance());
+        
+    });
+
+    it("getBalance of Account and withdraw ", function(){
+        acc.withdraw(100);
+        assert.equal(200, acc.getBalance());
+        
+    });
+        
+});
+
+describe("Saving Account ", function(){
+
+    let saving = new Savingsaccount(2);
+
+    it("getInterest of SavingAccount", function(){
+        assert.equal(5, saving.getInterest());
+    });
+
+    it("setInterest of SavingAccount", function(){
+        saving.setInterest(7);
+        assert.equal(7, saving.getInterest());
+    });
+
+    it("addInterest of SavingAccount", function(){
+        saving.deposit(250);
+        saving.addInterest();
+
+        assert.equal(267.5, saving.getBalance());
+    });
+
+    it("endOfMonth of SavingAccount", function(){
+        assert.equal("Interest added SavingsAccount:"+ saving.getNumber() +" balance: "+ saving.getBalance() +"interest:"+ (saving.getBalance() * saving._interest / 100),saving.endOfMonth());
+    });
+})
+
+
+
+
+
+
+
+
+describe("Checking Account", function(){
+
+    let checking=new CheckingAccount(3);
+
+    it("getOverdraft of CheckingAccount", function(){
+        
+        assert.equal(500, checking.getOverdraft());
+    });
+
+    it("setOverdraft of CheckingAccount", function(){
+        checking.setOverdraft(750);
+        checking.withdraw(500); 
+        assert.equal(250, checking.getOverdraft());
+    });
+
+    it("endOfMonth of CheckingAccount", function(){
+               
+        assert.equal("Warning, low balance CheckingAccount: "+ checking.getNumber() +"balance: "+ checking.getBalance()+" overdraft limit:"+checking._overdraft, checking.endOfMonth());
+    });
+
+})
+
+describe("Bank", function(){
+    
+    let bank=new Bank();
+
+    it("addAccount of Bank", function(){
+               
+        assert.equal(1, bank.addAccount());
+    });
+
+    it("addSavingsAccount of Bank", function(){
+               
+        assert.equal(2, bank.addSavingsAccount(6));
+    });
+
+    it("addCheckingAccount of Bank", function(){
+               
+        assert.equal(3, bank.addCheckingAccount(650));
+    });
+    
+    it("closeAccount of Bank", function(){
+               
+        assert.equal(2, bank.closeAccount(2));
+    });
+})
